07007935f858d53742db562f6f1945c7244debb6
tk4982 <tk4982@live.com>
2019-07-12 01:57:01 -0400

    new test
    new test

---
+++ ForeignLanguageTools/ForeignLanguageTool/src/UI/TestItem.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+/**
+ *
+ * @author Hyung Kang
+ */
+public class TestItem {
+    String name;
+    String value;
+    
+    public TestItem(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+    
+    @Override
+    public String toString() {
+        return this.name;
+    }
+    
+}
